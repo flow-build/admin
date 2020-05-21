0e56beb802e4d8d6b8712ce034dca05b3a59a111
Matheus Vieira <matheus.vs2015@hotmail.com>
2020-05-21 20:34:18 -0300

    Refactor Sidebar conflict merge

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,13 +1,16 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import MdContact from 'react-ionicons/lib/MdContact';
 
 
-const SideBar = () => (
+const SideBar = ({
+  children,
+}) => (
   <div className="sidebar">
     <div className="sidebar-user">
       <MdContact className="sidebar-icon" />
       <p>User</p>
-      {/* SIDEBARITEM */}
+      {children}
     </div>
   </div>
 );
