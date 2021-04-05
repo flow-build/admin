1cb48e14a7ac38be60ba390ebd67cdeb239791f4
Tito <tito.carvalho@fdte.io>
2021-04-05 09:39:59 -0300

    feat(generalstats): addition of filtering elements

--- src/pages/GeneralStats/GeneralStats.jsx
+++ src/pages/GeneralStats/GeneralStats.jsx
@@ -9,18 +9,78 @@ import * as S from './styles'
 
 const GeneralStats = ({ className, ...props }) => {
   const [generalStatsData, setGeneralStatsData] = useState('')
+  const [searchString, setSearchString] = useState('')
+  const [generalStatsDataDefault, setGeneralStatsDataDefault] = useState('')
 
   const loadData = async () => {
     const generalStatsResponse = await API.loadGeneralStats()
     UTIL.stringifyObjects(generalStatsResponse)
     setGeneralStatsData(generalStatsResponse)
+    setGeneralStatsDataDefault(generalStatsResponse)
+  }
+
+  const onSearchbarChangeHandler = (enteredText) => {
+    setSearchString(enteredText)
+
+    // console.log('[GeneralStats] searchString: ', searchString)
+    // console.log('[GeneralStats] e.target.value: ', enteredText)
+
+    const searchStringRegex = new RegExp(enteredText, 'i')
+    if (enteredText !== '') {
+      setGeneralStatsData(
+        [...generalStatsDataDefault].filter(
+          (el) =>
+            searchStringRegex.test(el.id) ||
+            searchStringRegex.test(el.name) ||
+            searchStringRegex.test(el.username) ||
+            searchStringRegex.test(el.address)
+        )
+      )
+    } else {
+      setGeneralStatsData(generalStatsDataDefault)
+    }
+  }
+
+  const handleDropdownChange = (value) => {
+    // console.log('[GeneralStats] value: ', value)
+    const searchStringRegex = new RegExp(options[value - 1]?.text, 'i')
+    if (options[value - 1]) {
+      setGeneralStatsData(
+        [...generalStatsDataDefault].filter(
+          (el) =>
+            searchStringRegex.test(el.id) ||
+            searchStringRegex.test(el.name) ||
+            searchStringRegex.test(el.username) ||
+            searchStringRegex.test(el.address)
+        )
+      )
+    } else {
+      setGeneralStatsData(generalStatsDataDefault)
+    }
   }
 
   useEffect(() => loadData(), [])
 
+  const options = [
+    { value: '1', text: 'Bret' },
+    { value: '2', text: 'Delphine' }
+  ]
+
   return (
     <S.Container>
       <h1>General Stats</h1>
+      <C.UI.TextField
+        name="name"
+        placeholder="Numero"
+        // type="text"
+        onChange={(e) => onSearchbarChangeHandler(e.target.value)}
+        value={searchString}
+      />
+      <C.UI.Dropdown
+        label={'Dropdown menu'}
+        options={options}
+        onChange={(e) => handleDropdownChange(e.target.value)}
+      />
       <S.Content className={className} {...props}>
         <C.GRID.GeneralStatsGrid rowData={generalStatsData} />
       </S.Content>
